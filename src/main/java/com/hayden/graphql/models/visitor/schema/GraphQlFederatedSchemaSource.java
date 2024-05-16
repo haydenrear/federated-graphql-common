76dafb3493451a41b176336bd5b1ab0eaedfb9dc
hayden.rear <hayden.rear@gmail.com>
2024-05-16 16:42:22 -0400

    changed ordering of pointwise Updated injection to fix profiles and fixed injection for recursive fish Injection Injection  changed ordering of pointwise Updated injection to fix profiles and fixed injection for recursive fish Injection Injection

--- src/main/java/com/hayden/graphql/models/visitor/schema/GraphQlFederatedSchemaSource.java
+++ src/main/java/com/hayden/graphql/models/visitor/schema/GraphQlFederatedSchemaSource.java
@@ -1,10 +1,14 @@
 package com.hayden.graphql.models.visitor.schema;
 
 import com.hayden.graphql.models.GraphQlTarget;
+import com.hayden.utilitymodule.RandomUtils;
 
 /**
  * A services GraphQl schema to be proxied using GraphQl federation.
  *
  */
-public record GraphQlFederatedSchemaSource(GraphQlTarget targetType, String target) {
+public record GraphQlFederatedSchemaSource(GraphQlTarget targetType, String target, String schemaName) {
+    public GraphQlFederatedSchemaSource(GraphQlTarget targetType, String target) {
+        this(targetType, target, "%s.graphql".formatted(RandomUtils.randomNumberString(10)));
+    }
 }
